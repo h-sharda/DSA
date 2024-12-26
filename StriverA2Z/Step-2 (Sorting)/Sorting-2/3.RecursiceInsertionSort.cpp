3f4e5f78ad65d53a5d67722d50e72080d9da34bd
Vector <harshitsharda0@gmail.com>
2024-12-27 02:30:13 +0530

    sorting

---
+++ StriverA2Z/Step-2 (Sorting)/Sorting-2/3.RecursiceInsertionSort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fast_io() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+void rInsertionSort(vector<int>& v, int n) {
+    
+}
+
+int main() {
+    fast_io();
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n, 0);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        
+        rInsertionSort(v, n);
+
+        for (int i = 0; i < v.size(); i++) cout << v[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
