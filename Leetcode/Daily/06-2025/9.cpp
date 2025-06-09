05b19c1eabcfe13bae766b561a5d1c572ea39573
Vector <143745200+h-sharda@users.noreply.github.com>
2025-06-10 00:57:15 +0530

    Add solution: 440. K-th Smallest in Lexicographical Order

--- Leetcode/Daily/06-2025/9.cpp
+++ Leetcode/Daily/06-2025/9.cpp
@@ -1,6 +1,9 @@
+// Problem: 440. K-th Smallest in Lexicographical Order (Hard)
+// Link: https://leetcode.com/problems/k-th-smallest-in-lexicographical-order
+
 class Solution {
 public:
     int findKthNumber(int n, int k) {
-        huihiuhuihhhuhuhihiuhihu
+        
     }
 };
