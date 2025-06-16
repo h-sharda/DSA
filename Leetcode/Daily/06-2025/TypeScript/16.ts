453577f8381948f80d120f9f22d8ffd08e3521f6
Vector <143745200+h-sharda@users.noreply.github.com>
2025-06-16 05:55:15 +0530

    Daily Solution (16-06-2025): 2016. Maximum Difference Between Increasing Elements

---
+++ Leetcode/Daily/06-2025/TypeScript/16.ts
@@ -0,0 +1,13 @@
+// Problem: 2016. Maximum Difference Between Increasing Elements (Easy)
+// Link: https://leetcode.com/problems/maximum-difference-between-increasing-elements
+
+function maximumDifference(nums: number[]): number {
+    let n = nums.length, maxx = 0, ans = -1;
+
+    for (let i = n-1; i >= 0; i--) {
+        if (maxx > nums[i]) ans = Math.max(ans, maxx - nums[i]);
+        maxx = Math.max(maxx, nums[i]);
+    }
+
+    return ans;    
+};
