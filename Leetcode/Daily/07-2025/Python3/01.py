1053d885be5726ea912bd723ce1fd43833707f86
Vector <143745200+h-sharda@users.noreply.github.com>
2025-07-01 11:40:12 +0530

    Daily Solution (01-07-2025): 3330. Find the Original Typed String I

---
+++ Leetcode/Daily/07-2025/Python3/01.py
@@ -0,0 +1,13 @@
+// Problem: 3330. Find the Original Typed String I (Easy)
+// Link: https://leetcode.com/problems/find-the-original-typed-string-i
+
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+        ans = 1
+        n = len(word)
+
+        for i in range (1, n):
+            if word[i] == word[i-1]:
+                ans += 1
+        
+        return ans
