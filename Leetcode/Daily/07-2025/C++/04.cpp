38ec4871f955cc731784ef40325c67749d15fb51
Vector <143745200+h-sharda@users.noreply.github.com>
2025-07-04 14:10:43 +0530

    Daily Solution (04-07-2025): 3307. Find the K-th Character in String Game II

---
+++ Leetcode/Daily/07-2025/C++/04.cpp
@@ -0,0 +1,28 @@
+// Problem: 3307. Find the K-th Character in String Game II (Hard)
+// Link: https://leetcode.com/problems/find-the-k-th-character-in-string-game-ii
+
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        int ops = 0, i = 0;
+
+        while (k % 2 == 0) {
+            ops += operations[i];
+            i++;
+            k /= 2;
+        }
+
+        i++;
+        k /= 2;
+
+        while ( k > 0) {
+            if (k % 2 == 1) {
+                ops += operations[i];
+            }
+            i++;
+            k /= 2;
+        }
+
+        return ops % 26 + 'a';
+    }
+};
