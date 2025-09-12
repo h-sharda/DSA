7354329653c4cb47c261f0ac8b8eec31873552f0
Vector <143745200+h-sharda@users.noreply.github.com>
2025-09-12 07:38:04 +0530

    Add Solution: 2327. Number of People Aware of a Secret

--- Leetcode/Daily/09-2025/C++/12.cpp
+++ Leetcode/Daily/09-2025/C++/12.cpp
@@ -1,10 +1,19 @@
-// Problem: 3227. Vowels Game in a String (Medium)
-// Link: https://leetcode.com/problems/vowels-game-in-a-string
+// Problem: 2327. Number of People Aware of a Secret (Medium)
+// Link: https://leetcode.com/problems/number-of-people-aware-of-a-secret
 
 class Solution {
 public:
-    bool doesAliceWin(string s) {
-        for (char &ch: s) if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') return true;
-        return false;
+    int MOD = 1e9 + 7;
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<int> v(n + 1);
+        v[1] = 1;
+
+        for (int i = 1; i <= n; i++) 
+            for (int j = i + delay; j < i + forget && j <= n; j++) v[j] = (v[j] + v[i]) % MOD;
+
+        int ans = 0;
+        for (int i = n - forget + 1; i <= n; i++) ans = (ans + v[i]) % MOD;
+
+        return ans;
     }
 };
