bc07305c37d9429d219efc2771fba696c978b1b3
Vector <143745200+h-sharda@users.noreply.github.com>
2025-11-15 02:10:49 +0530

    Add Solution: 2536. Increment Submatrices by One

--- Leetcode/Daily/11-2025/C++/14.cpp
+++ Leetcode/Daily/11-2025/C++/14.cpp
@@ -4,19 +4,27 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> ans(n, vector<int>(n + 1));
+        vector<vector<int>> diff(n + 1, vector<int>(n + 1));
+        vector<vector<int>> ans(n, vector<int>(n));
 
         for (auto &it: queries) {
             int x1 = it[0], y1 = it[1], x2 = it[2], y2 = it[3];
-            for (int i = x1; i <= x2; i++) {
-                ans[i][y1]++;
-                ans[i][y2 + 1]--;
-            }
+            
+            diff[x1][y1]++;
+            diff[x1][y2 + 1]--;
+            diff[x2 + 1][y1]--;
+            diff[x2 + 1][y2 + 1]++;            
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < n; j++) ans[i][j] += ans[i][j - 1];
-            ans[i].pop_back();
+        ans[0][0] = diff[0][0];
+
+        for (int i = 1; i < n; i++) ans[i][0] = diff[i][0] + ans[i-1][0];
+        for (int j = 1; j < n; j++) ans[0][j] = diff[0][j] + ans[0][j-1];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                ans[i][j] = diff[i][j] + ans[i-1][j] + ans[i][j-1] - ans[i-1][j-1];
+            }
         }
 
         return ans;
